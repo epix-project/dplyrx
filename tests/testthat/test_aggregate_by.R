44f9cff782597cc25bfc607829fa01acc7153704
Lucie Contamin <contamin.lucie@gmail.com>
2018-09-27 10:56:55 +0700

    add tests for aggregate_by

--- tests/testthat/test_aggregate_by.R
+++ tests/testthat/test_aggregate_by.R
@@ -1,7 +1,10 @@
 library(dplyr)    # for 'recode', 'select', 'summarise'
 library(magrittr) # for ' %>% '
 
-test_that("`aggregate` returns the correct output", {
+context("test `gaggregate_by` output")
+
+test_that("`aggregate_by` returns the correct output with columns name
+          imputed", {
 
   # create a simple data frame
   set.seed(30101976)
@@ -41,4 +44,52 @@ test_that("`aggregate` returns the correct output", {
 
   expect_equal(expect,test3)
 
-  })
+})
+
+
+test_that("`aggregate_by` returns the correct output with columns name
+          imputed", {
+
+  # create a simple data frame
+  set.seed(30101976)
+  data <- expand.grid(letters[1:3], 1:3, 4:6)
+  data$Var4 <- sample(1:100, nrow(data), TRUE)
+  data$Var5 <- sample(1:100, nrow(data), TRUE)
+  data$Var6 <- sample(1:100, nrow(data), TRUE)
+
+   # expected results
+   sum_all <- data %>%
+     mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
+     summarise_all(sum)
+   mean_all <- data %>%
+     mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
+     summarise_all(mean)
+
+   # Test with one sum function
+   test4 <- data %>%
+     mutate(Var1 = recode(Var1, a = "b")) %>%
+     aggregate_by(Var1, Var2, Var3, .funs = sum)
+   expect_equal(test4, sum_all)
+
+   # Test with one mean function
+   test5 <- data %>%
+     mutate(Var1 = recode(Var1, a = "b")) %>%
+     aggregate_by(Var1, Var2, Var3, .funs = mean)
+   expect_equal(test5, mean_all)
+
+   # Test with both mean and sum function
+   colnames(sum_all) %<>% gsub("Var4", "Var4_sum", .) %>%
+     gsub("Var5", "Var5_sum", .) %>%
+     gsub("Var6", "Var6_sum", .)
+   colnames(mean_all) %<>% gsub("Var4", "Var4_mean", .) %>%
+     gsub("Var5", "Var5_mean", .) %>%
+     gsub("Var6", "Var6_mean", .)
+   expect <- list(sum_all, mean_all) %>%
+     purrr::reduce(left_join, by = c("Var1", "Var2", "Var3"))
+
+   test6 <- data %>%
+     mutate(Var1 = recode(Var1, a = "b")) %>%
+     aggregate_by(Var1, Var2, Var3, .funs = list(sum, mean))
+   expect_equal(test6, expect)
+
+})
