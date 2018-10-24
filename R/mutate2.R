610843ec318adc1e967e4e507ad8663f833146d0
Lucie Contamin <contamin.lucie@gmail.com>
2018-10-24 09:17:14 +0700

    keep row.names attributes in mutate2

--- R/mutate2.R
+++ R/mutate2.R
@@ -32,15 +32,23 @@
 #' @export
 #'
 mutate2 <- function(.data, ...) {
+
   old_attributes <- attributes(.data)
   old_class <- class(.data)
   .data %<>% mutate(...)
+
   new_attributes <- attributes(.data)
   missing_attributes <- old_attributes[! names(old_attributes) %in%
                                          names(new_attributes)]
-  if (length(missing_attributes) > 0)
+  if (length(missing_attributes) > 0) {
     for (i in seq_along(missing_attributes))
       attr(.data, names(missing_attributes)[i]) <- missing_attributes[[i]]
+  }
+
+  if (length(old_attributes$row.names) > 0) {
+    attr(.data, "row.names") <-  old_attributes$row.names
+  }
+
   class(.data) <- old_class
   .data
 }
