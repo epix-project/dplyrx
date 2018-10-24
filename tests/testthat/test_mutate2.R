e441d986f6468e69322484d9bec2c4c1f92339cf
Lucie Contamin <contamin.lucie@gmail.com>
2018-10-24 09:17:26 +0700

    add tests for mutate2

---
+++ tests/testthat/test_mutate2.R
@@ -0,0 +1,14 @@
+library(magrittr) # for ' %>% '
+library(dplyr)
+
+context("`mutate2`")
+
+test_that("`mutate2` returns the correct output", {
+
+  attr(mtcars, "my_attribute_name") <- "my_attribute_value"
+
+  expect_equivalent(mtcars %>% mutate2(gear = gear), mtcars)
+
+  expect_s3_class(mtcars %>% mutate2(gear = gear), class(mtcars))
+
+})
