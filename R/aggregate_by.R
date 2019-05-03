8f868891ccf62a7a309640b3f4d2691beca9bfd3
Lucie Contamin <contamin.lucie@gmail.com>
2019-05-03 16:18:36 +0700

    Update to remove dependency (keep rlang)

--- R/aggregate_by.R
+++ R/aggregate_by.R
@@ -1,13 +1,41 @@
+#' Apply agregating function
+#'
+#'@param df the data frame on which to perform the aggregation.
+#' @param .funcs function(s) to apply.
+#' @param col names/position of the column where the \code{.funcs} be apply.
+#' @param group_var names of the column grouped to aggregate.
+#' @noRd
+apply_fct <- function(df, .funcs, col, group_var) {
+
+   if (length(.funcs) > 1) {
+    df <- lapply(.funcs, function(x) {
+      group_cal <- df[col]
+      df <- aggregate(group_cal,
+                      by = as.list(df[group_var]),
+                      FUN = x)
+      names(df)[which(names(df) %in% names(group_cal))] <-
+        paste0(names(df)[which(names(df) %in% names(group_cal))], "_", x)
+      df
+    })
+    df <- Reduce(merge, df)
+  } else {
+    df <- aggregate(df[col],
+                    by = as.list(df[group_var]),
+                    FUN = .funcs)
+    names(df)
+  }
+  df <- df[do.call(order, df[,group_var, drop = TRUE]), ]
+  df
+}
+
+#-------------------------------------------------------------------------------
 #' Aggregating observations on some variables
 #'
 #' \code{aggregate_by} aggregates some variables of a data frame according to
 #' the values of a categorical variable of the same data frame and using a given
 #' function for the aggregation.
 #'
-#' On a data frame that contains the values of the categorical variable to
-#' aggregate (\code{col_name}, \code{...}), the function performs a
-#' \code{\link[dplyr]{group_by}} followed by a \code{\link[dplyr]{summarise}}
-#' with the function(s) \code{.funs}.\cr
+#' The function does not support factors.
 #' The \code{.funs} arguments can be inputed with or without quotation as one
 #' function, for example: \code{.funs = sum} or \code{.funs = "sum"} or a list
 #' of multiple function, for example: \code{.funs = list(sum, mean)} or
@@ -18,7 +46,7 @@
 #'
 #' @param df the data frame on which to perform the aggregation.
 #' @param col_name the variable on which to perform the aggregation.
-#' @param ... others variables passed on to `group_by`.
+#' @param ... others variables passed on to \code{group_by}.
 #' @param .funs the name of a function given as a name, literal character
 #' string or a list of names or character strings. See \code{Details} for
 #' more information on the specific usage. By default \code{sum}.
@@ -28,45 +56,10 @@
 #'
 #' @author Marc Choisy and Lucie Contamin.
 #'
-#' @examples
+#' @example inst/examples/aggregate_by.R
 #'
-#' library(dplyr)
-#'
-#' ## A toy data frame:
-#' set.seed(30101976)
-#' data <- expand.grid(letters[1:3], 1:3, 4:6)
-#' data$Var4 <- sample(1:100, nrow(data), TRUE)
-#' data$Var5 <- sample(1:100, nrow(data), TRUE)
-#' data$Var6 <- sample(1:100, nrow(data), TRUE)
-#'
-#' ## Aggregating the values "a" and "b" of the categorical variable Var1,
-#' ## summing the values of variables Var4, Var5, Var6 (i.e. all the variables
-#' ## that are not in the arguments of the function call):
-#' data %>%
-#'   mutate(Var1 = recode(Var1, a = "b")) %>%
-#'   aggregate_by(Var1, Var2, Var3)
-#'
-#' ## To calculate the mean value:
-#' data %>%
-#'   mutate(Var1 = recode(Var1, a = "b")) %>%
-#'   aggregate_by(Var1, Var2, Var3, .funs = mean)
-#'
-#' ## or to apply it to all the columns:
-#' data %>%
-#'   mutate(Var1 = recode(Var1, a = "b")) %>%
-#'   aggregate_by(Var1, Var2, Var3, .funs = list(sum, mean))
-#'
-#' ## To calculate the mean and the sum:
-#' data %>%
-#'   mutate(Var1 = recode(Var1, a = "b")) %>%
-#'   aggregate_by(Var1, Var2, Var3, .funs = list(sum(Var6), mean(Var4)))
-#'
-#' @importFrom magrittr %>% %<>%
-#' @importFrom dplyr filter anti_join mutate_if bind_rows select group_by
-#' summarise_all mutate enquo summarise left_join ungroup
-#' @importFrom purrr reduce
-#' @importFrom rlang parse_expr
-#' @importFrom stringr str_detect
+#' @importFrom stats aggregate
+#' @importFrom rlang parse_expr enquo
 #'
 #' @export
 aggregate_by <- function(df, col_name, ..., .funs = sum) {
@@ -78,71 +71,72 @@ aggregate_by <- function(df, col_name, ..., .funs = sum) {
 
   res <- try(eval(...), silent = TRUE)
   if (inherits(res, "try-error")) {
-    col_sel <-  as.character(substitute(list(...))) %>%
-      grep("list", ., invert = TRUE, value = TRUE)
+    col_sel <-  as.character(substitute(list(...)))
+    col_sel <-  grep("list", col_sel, invert = TRUE, value = TRUE)
   } else {
     res1 <- try(eval(substitute(...)), silent = TRUE)
     if (inherits(res1, "try-error")) {
-      col_sel <- list(...) %>% unlist()
+      col_sel <- unlist(list(...))
     } else {
-      col_sel <- eval(substitute(list(...))) %>% unlist() %>% as.vector()
+      col_sel <- eval(substitute(list(...)))
+      col_sel <- as.vector(unlist(col_sel))
     }
   }
 
   group_var <-  c(col_name, col_sel)
 
-  funcs <- as.character(substitute(.funs)) %>%
-    grep("list", ., invert = TRUE, value = TRUE) %>%
-    unlist()
+  funcs <- as.character(substitute(.funs))
+  funcs <- unlist(grep("list", funcs, invert = TRUE, value = TRUE))
 
-  test <- funcs %>% as.character() %>% unlist() %>%
-    strsplit("\\, |\\(|\\)") %>% unlist()
-  sel <- test %>% stringr::str_detect(paste(names(df), collapse = "|"))
+  test <- as.character(funcs)
+  test <- unlist(test)
+  test <- unlist(strsplit(test, "\\, |\\(|\\)"))
+
+  sel <- grepl(paste(names(df), collapse = "|"), test)
   func_res <- try(lapply(test[!sel], function(x) match.fun(x)), silent = TRUE)
 
   if (inherits(func_res, "try-error")) {
     funcs <- unlist(.funs)
   }
 
-  if (funcs %>% is.element(names(df)) %>% any()) {
+  df <- as.data.frame(df, stringsAsFactors = FALSE)
 
-    x <- enquo(.funs)
-    df %<>% group_by(.dots = group_var) %>%
-      summarise(!!! x)
+  if (any(is.element(funcs, names(df)))) {
 
-  } else if (grepl(paste(names(df), collapse = "|"), funcs) %>% any == FALSE) {
+   x <- rlang::enquo(.funs)
+   sel <- names(df)[names(df) %in% funcs]
+   funcs <- funcs[!funcs %in% names(df)]
+   df <- apply_fct(df, funcs, sel, group_var)
 
-    df %<>% group_by(.dots = group_var) %>%
-      summarise_all(funcs)
+  } else if (any(grepl(paste(names(df), collapse = "|"), funcs)) == FALSE) {
+
+    sel <- -which(colnames(df) %in% group_var)
+    df <- apply_fct(df, funcs, sel, group_var)
 
   } else {
 
     df <- lapply(funcs, function(x) {
 
-      if (grepl("\\,", x)){
+     if (grepl("\\,", x)){
+       x <- as.character(x)
+       sel <- unlist(strsplit(x, ", |,"))
+       sel <- unlist(strsplit(sel, "\\("))
+       sel <- gsub("[[:punct:]]", "", sel)
 
-        x <- as.character(x)
-        sel <- strsplit(x, ", |,") %>% unlist() %>% strsplit("\\(") %>% unlist()
-        msel <- sel[1]
-        x <- paste0(sel, ")", sep = "") %>% gsub("))", ")", .) %>%
-          paste(msel, ., sep = "(") %>% .[-1] %>% unlist()
+       fct_sel <- sel[1]
+       col_sel <- names(df)[names(df) %in% sel]
+       df <- apply_fct(df, fct_sel, col_sel, group_var)
 
-        df <- lapply(x, function(z) {
+      } else {
 
-          z <- rlang::parse_expr(z)
-          df %<>% group_by(.dots = group_var) %>%
-            summarise(!! z)
-        }) %>%
-          reduce(left_join, by = group_var)
-
-        } else {
-
-        x <- rlang::parse_expr(x)
-        df %<>% group_by(.dots = group_var) %>%
-          summarise(!! x)
+          x <- unlist(strsplit(x, "[[:punct:]]"))
+          sel <- names(df)[names(df) %in% x]
+          fun_s <- x[!x %in% names(df)]
+          df <- apply_fct(df, fun_s, sel, group_var)
       }
-    }) %>%
-      reduce(left_join, by = group_var)
+    })
+   df <-  Reduce(merge, df)
   }
-  df %>% ungroup
+  df <- df[do.call(order, df[,group_var, drop = TRUE]), ]
+
 }
--- tests/testthat/test_aggregate_by.R
+++ tests/testthat/test_aggregate_by.R
@@ -1,191 +1,110 @@
-library(dplyr)    # for 'recode', 'select', 'summarise'
-library(magrittr) # for ' %>% '
+# create a simple data frame
+set.seed(30101976)
+data <- expand.grid(letters[1:3], 1:3, 4:6)
+data$Var4 <- sample(1:100, nrow(data), TRUE)
+data$Var5 <- sample(1:100, nrow(data), TRUE)
+data$Var6 <- sample(1:100, nrow(data), TRUE)
+data2 <- transform(data, Var1 =  ifelse(data$Var1 == "a", "b",
+                                        as.character(data$Var1)))
+
+# expected results
+sum6 <- aggregate(Var6 ~ Var1  + Var2 + Var3, data = data2, FUN = sum)
+sum6 <- sum6[order(sum6$Var1, sum6$Var2, sum6$Var3), ]
+sum6 <- as.data.frame(sum6, stingsAsFactors = FALSE)
+mean4 <- aggregate(Var4 ~ Var1  + Var2 + Var3, data = data2, FUN = mean)
+mean4 <- mean4[order(mean4$Var1, mean4$Var2, mean4$Var3), ]
+mean4 <- as.data.frame(mean4, stingsAsFactors = FALSE)
+mean5 <- aggregate(Var5 ~ Var1  + Var2 + Var3, data = data2, FUN = mean)
+mean5 <- mean5[order(mean5$Var1, mean5$Var2, mean5$Var3), ]
+mean5 <- as.data.frame(mean5, stingsAsFactors = FALSE)
+expect <- list(mean4, mean5, sum6)
+expect <- Reduce(merge, expect)
+
+sum_all <- aggregate(. ~ Var1  + Var2 + Var3, data = data2, FUN = sum)
+sum_all <- sum_all[order(sum_all$Var1, sum_all$Var2, sum_all$Var3), ]
+sum_all <- as.data.frame(sum_all, stingsAsFactors = FALSE)
+
+mean_all <- aggregate(. ~ Var1  + Var2 + Var3, data = data2, FUN = mean)
+mean_all <- mean_all[order(mean_all$Var1, mean_all$Var2, mean_all$Var3), ]
+mean_all <- as.data.frame(mean_all, stingsAsFactors = FALSE)
+
+expect_all <- list(sum_all, mean_all)
+expect_all <- Reduce(
+  function(x, y) merge(x = x, y = y, by = c("Var1", "Var2", "Var3")),
+  expect_all)
+colnames(expect_all) <- gsub(".x", "_sum",colnames(expect_all))
+colnames(expect_all) <- gsub(".y", "_mean",colnames(expect_all))
 
 context("`aggregate_by`")
 
 test_that("`aggregate_by` returns the correct output with columns name
           imputed", {
 
-  # create a simple data frame
-  set.seed(30101976)
-  data <- expand.grid(letters[1:3], 1:3, 4:6)
-  data$Var4 <- sample(1:100, nrow(data), TRUE)
-  data$Var5 <- sample(1:100, nrow(data), TRUE)
-  data$Var6 <- sample(1:100, nrow(data), TRUE)
-
-  # expected results
-  sum6 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
-    summarise(sum(Var6))
-  mean4 <- data %>% mutate(Var1 = recode(Var1, a = "b")) %>%
-    group_by(Var1, Var2, Var3) %>% summarise(mean(Var4))
-  mean5 <- data %>% mutate(Var1 = recode(Var1, a = "b")) %>%
-    group_by(Var1, Var2, Var3) %>% summarise(mean(Var5))
-  expect <- list(mean4, mean5, sum6) %>% purrr::reduce(left_join)
-
   # Test with one function
-  test1 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>%
-    aggregate_by(Var1, Var2, Var3, .funs = sum(Var6))
-
-  testthat::expect_equal(test1, expect %>% select(-`mean(Var5)`, -`mean(Var4)`))
+  test1 <- aggregate_by(data2, Var1, Var2, Var3, .funs = sum(Var6))
+  testthat::expect_equal(test1, sum6)
 
   # Test with two operations
-  test2 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>%
-    aggregate_by(Var1, Var2, Var3, .funs = list(sum(Var6), mean(Var4)))
-
-  testthat::expect_equal(test2, expect %>% select(-`mean(Var5)`))
+  test2 <- aggregate_by(data2, Var1, Var2, Var3,
+                        .funs = list(sum(Var6), mean(Var4)))
+  testthat::expect_equal(test2, Reduce(merge, list(sum6, mean4)))
 
   # Test with two operations and on one two columns
-  test3 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>%
-    aggregate_by(Var1, Var2, Var3, .funs = list(mean(Var4, Var5), sum(Var6)))
-
-  testthat::expect_equal(expect, test3)
-
+  test3 <- aggregate_by(data2, Var1, Var2, Var3,
+                        .funs = list(mean(Var4, Var5), sum(Var6)))
+  testthat::expect_equal(test3, expect)
 })
 
-
 test_that("`aggregate_by` returns the correct output without columns name
-          imputedin `funs`", {
-
-  # create a simple data frame
-  set.seed(30101976)
-  data <- expand.grid(letters[1:3], 1:3, 4:6)
-  data$Var4 <- sample(1:100, nrow(data), TRUE)
-  data$Var5 <- sample(1:100, nrow(data), TRUE)
-  data$Var6 <- sample(1:100, nrow(data), TRUE)
-
-   # expected results
-   sum_all <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
-     summarise_all(sum)
-   mean_all <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
-     summarise_all(mean)
+          imputed in `funs`", {
 
    # Test with one sum function
-   test4 <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>%
-     aggregate_by(Var1, Var2, Var3, .funs = sum)
+   test4 <- aggregate_by(data2, Var1, Var2, Var3, .funs = sum)
    testthat::expect_equal(test4, sum_all)
 
    # Test with one mean function
-   test5 <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>%
-     aggregate_by(Var1, Var2, Var3, .funs = mean)
+   test5 <- aggregate_by(data2, Var1, Var2, Var3, .funs = mean)
    testthat::expect_equal(test5, mean_all)
 
    # Test with both mean and sum function
-   colnames(sum_all) %<>% gsub("Var4", "Var4_sum", .) %>%
-     gsub("Var5", "Var5_sum", .) %>%
-     gsub("Var6", "Var6_sum", .)
-   colnames(mean_all) %<>% gsub("Var4", "Var4_mean", .) %>%
-     gsub("Var5", "Var5_mean", .) %>%
-     gsub("Var6", "Var6_mean", .)
-   expect <- list(sum_all, mean_all) %>%
-     purrr::reduce(left_join, by = c("Var1", "Var2", "Var3"))
-
-   test6 <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>%
-     aggregate_by(Var1, Var2, Var3, .funs = list(sum, mean))
-   testthat::expect_equal(test6, expect)
+   test6 <- aggregate_by(data2, Var1, Var2, Var3, .funs = list(sum, mean))
+   testthat::expect_equal(test6, expect_all)
 
 })
-
 
 test_that("`aggregate_by` returns the correct output with quotation or not for
 columns name imputed", {
 
-  # create a simple data frame
-  set.seed(30101976)
-  data <- expand.grid(letters[1:3], 1:3, 4:6)
-  data$Var4 <- sample(1:100, nrow(data), TRUE)
-  data$Var5 <- sample(1:100, nrow(data), TRUE)
-  data$Var6 <- sample(1:100, nrow(data), TRUE)
-
-   # expected results
-   sum_all <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
-     summarise_all(sum)
-
    # Test without quotation
-   test7 <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>%
-     aggregate_by(Var1, Var2, Var3)
+   test7 <- aggregate_by(data2, Var1, Var2, Var3)
    testthat::expect_equal(test7, sum_all)
 
    # Test with quotation
-   test8 <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>%
-     aggregate_by("Var1", "Var2", "Var3")
+   test8 <- aggregate_by(data2, "Var1", "Var2", "Var3")
    testthat::expect_equal(test8, sum_all)
 
    # Test with a vector
    sel <- c("Var2", "Var3")
-   test8 <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>%
-     aggregate_by(Var1, sel)
+   test8 <- aggregate_by(data2, Var1, sel)
    testthat::expect_equal(test8, sum_all)
 
    # Test with a vector
    sel <- c("Var1", "Var2", "Var3")
-   test9 <- data %>%
-     mutate(Var1 = recode(Var1, a = "b")) %>%
-     aggregate_by(sel)
+   test9 <- aggregate_by(data2, sel)
    testthat::expect_equal(test9, sum_all)
 
 })
 
 test_that("`aggregate_by` behaviours during programming", {
 
-  # create a simple data frame
-  set.seed(30101976)
-  data <- expand.grid(letters[1:3], 1:3, 4:6)
-  data$Var4 <- sample(1:100, nrow(data), TRUE)
-  data$Var5 <- sample(1:100, nrow(data), TRUE)
-  data$Var6 <- sample(1:100, nrow(data), TRUE)
-
-  # expected results
-  sum_all <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
-    summarise_all(sum)
-  mean_all <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
-    summarise_all(mean)
-
   # Test inside a function
   do_aggregate <- function(df, group_var){
     df <- aggregate_by(df, Var1, group_var)
-    df
   }
 
   group_var <- c("Var2", "Var3")
-  test10 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>%
-    do_aggregate(group_var)
+  test10 <- do_aggregate(data2, group_var)
   testthat::expect_equal(test10, sum_all)
-
-  # expect results with both mean and sum function
-  colnames(sum_all) %<>% gsub("Var4", "Var4_sum", .) %>%
-    gsub("Var5", "Var5_sum", .) %>%
-    gsub("Var6", "Var6_sum", .)
-  colnames(mean_all) %<>% gsub("Var4", "Var4_mean", .) %>%
-    gsub("Var5", "Var5_mean", .) %>%
-    gsub("Var6", "Var6_mean", .)
-  expect_all <- list(sum_all, mean_all) %>%
-    purrr::reduce(left_join, by = c("Var1", "Var2", "Var3"))
-
-  # expected complexe results
-  sum6 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>% group_by(Var1, Var2, Var3) %>%
-    summarise(sum(Var6))
-  mean4 <- data %>% mutate(Var1 = recode(Var1, a = "b")) %>%
-    group_by(Var1, Var2, Var3) %>% summarise(mean(Var4))
-  mean5 <- data %>% mutate(Var1 = recode(Var1, a = "b")) %>%
-    group_by(Var1, Var2, Var3) %>% summarise(mean(Var5))
-  expect <- list(mean4, mean5, sum6) %>% purrr::reduce(left_join)
 
   # Test2 inside a function
   do_aggregate2 <- function(df, col_name, group_var, functs){
@@ -194,15 +113,10 @@ test_that("`aggregate_by` behaviours during programming", {
 
   sel <- c("Var2", "Var3")
   funs1 <- list("sum", "mean")
-  test11 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>%
-    do_aggregate2("Var1", sel, funs1)
+  test11 <- do_aggregate2(data2, "Var1", sel, funs1)
   testthat::expect_equal(test11, expect_all)
 
   funs2 <- list("mean(Var4, Var5)", "sum(Var6)")
-  test12 <- data %>%
-    mutate(Var1 = recode(Var1, a = "b")) %>%
-    do_aggregate2("Var1", sel, funs2)
+  test12 <- do_aggregate2(data2, "Var1", sel, funs2)
   testthat::expect_equal(test12, expect)
-
 })
