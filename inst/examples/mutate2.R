81236f7608f345555912b09c477cfd18537cd1db
Lucie Contamin <contamin.lucie@gmail.com>
2019-05-03 16:19:05 +0700

    add examples into inst/

---
+++ inst/examples/mutate2.R
@@ -0,0 +1,11 @@
+# Let's start by adding an attribute to a data frame:
+attr(mtcars, "my_attribute_name") <- "my_attribute_value"
+
+# We can see the attributes of this data frame:
+attributes(mtcars)
+
+# We can now see that mutate drops the non-class attributes:
+mtcars %>% mutate(a_new_variable = 3) %>% attributes()
+
+# Whereas mutate2 does not:
+mtcars %>% mutate2(a_new_variable = 3) %>% attributes()
