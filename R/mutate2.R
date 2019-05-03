b090bc2c4d539f1d40d2cdd3ddcc78d343662fbf
Lucie Contamin <contamin.lucie@gmail.com>
2019-05-03 16:08:52 +0700

    Remove magrittr dependency

--- R/mutate2.R
+++ R/mutate2.R
@@ -13,21 +13,9 @@
 #'
 #' @seealso \code{\link[dplyr]{mutate}}.
 #'
-#' @examples
-#' # Let's start by adding an attribute to a data frame:
-#' attr(mtcars, "my_attribute_name") <- "my_attribute_value"
-#'
-#' # We can see the attributes of this data frame:
-#' attributes(mtcars)
-#'
-#' # We can now see that mutate drops the non-class attributes:
-#' mtcars %>% mutate(a_new_variable = 3) %>% attributes()
-#'
-#' # Whereas mutate2 does not:
-#' mtcars %>% mutate2(a_new_variable = 3) %>% attributes()
+#' @example inst/examples/mutate2.R
 #'
 #' @importFrom dplyr mutate
-#' @importFrom magrittr %<>%
 #'
 #' @export
 #'
@@ -35,7 +23,7 @@ mutate2 <- function(.data, ...) {
 
   old_attributes <- attributes(.data)
   old_class <- class(.data)
-  .data %<>% mutate(...)
+  .data <- mutate(.data, ...)
 
   new_attributes <- attributes(.data)
   missing_attributes <- old_attributes[! names(old_attributes) %in%
