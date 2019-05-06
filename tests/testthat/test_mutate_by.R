d5ca3c70075376cc5ac89ad3f2f30720db38193d
Lucie Contamin <contamin.lucie@gmail.com>
2019-05-06 10:45:04 +0700

    Remove test1 mutate_by

--- tests/testthat/test_mutate_by.R
+++ tests/testthat/test_mutate_by.R
@@ -11,44 +11,30 @@ context("`mutate_by`")
 test_that("`mutate_by` returns the correct output", {
 
   expected <- data
-  expected[which(expected$Var4 > 75), "Var4"] <-
-    mean(expected[which(expected$Var2 == 3 & expected$Var1 == "a" &
-                          expected$Var4 <= 75), "Var4"])
+  expected[which(expected$Var4 > 75), "Var4"] <-  mean(data$Var4)
 
-  test1 <- mutate_by(data, Var4 > 75, mean, colgroups = c("Var1", "Var2"))
+  test1 <- mutate_by(data, Var4 > 75, mean)
 
-  # testthat::expect_equal(test1$Var4,
-  #                        expected[order(expected$Var1, expected$Var2), "Var4"])
-
-  # testthat::expect_equal(round(test1$Var4, 2),
-  #                        round(expected[order(expected$Var1, expected$Var2), "Var4"], 2))
-
-
-  expected2 <- data
-  expected2[which(expected2$Var4 > 75), "Var4"] <-  mean(data$Var4)
-
-  test2 <- mutate_by(data, Var4 > 75, mean)
-
-  testthat::expect_equal(expected2[order(expected2$Var1, expected2$Var2),],
-                         test2[order(test2$Var1, test2$Var2),])
+  testthat::expect_equal(expected[order(expected$Var1, expected$Var2),],
+                         test1[order(test1$Var1, test1$Var2),])
 
   # test arguments `...`
   data$Var4 <- c(sample(1:100, nrow(data) - 2, TRUE), NA, NA)
-  expected3 <- data
-  expected3[which(expected3$Var4 > 75), "Var4"] <- mean(data$Var4, na.rm = TRUE)
+  expected2 <- data
+  expected2[which(expected2$Var4 > 75), "Var4"] <- mean(data$Var4, na.rm = TRUE)
 
-  test3 <- mutate_by(data, Var4 > 75, mean, na.rm = TRUE)
-  testthat::expect_equal(expected3[order(expected3$Var1, expected3$Var2),],
-                         test3[order(test2$Var1, test3$Var2),])
+  test2 <- mutate_by(data, Var4 > 75, mean, na.rm = TRUE)
+  testthat::expect_equal(expected2[order(expected2$Var1, expected2$Var2),],
+                         test2[order(test2$Var1, test2$Var2),])
 
   # test all arguments
-  expected4 <- data
-  expected4[which(expected4$Var4 == 94), "Var4"] <-
-    mean(expected4[which(expected4$Var2 == 3 & expected4$Var1 == "c" &
-                         expected4$Var4 != 94), "Var4"])
+  expected3 <- data
+  expected3[which(expected3$Var4 == 94), "Var4"] <-
+    mean(expected3[which(expected3$Var2 == 3 & expected3$Var1 == "c" &
+                         expected3$Var4 != 94), "Var4"])
 
-  test4 <- mutate_by(data, Var4 == 94, mean, na.rm = TRUE,
+  test3 <- mutate_by(data, Var4 == 94, mean, na.rm = TRUE,
                      colgroups = c("Var1", "Var2"))
-  testthat::expect_equivalent(expected4[order(expected4$Var1, expected4$Var2),],
-                              test4)
+  testthat::expect_equivalent(expected3[order(expected3$Var1, expected3$Var2),],
+                              test3)
 })
